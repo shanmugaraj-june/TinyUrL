97bc6e2745a35224a716a0d3ee206da85ea304b5
shanmugarj-june <shanmugaraj4696@gmail.com>
2026-09-01 12:34:40 +0530

    feat: implement graceful server shutdown

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
 const app  = require("./app") ; 
-const {connectDB}  = require("./config/db") ;
+const {connectDB , mongoose}  = require("./config/db") ;
 const PORT = process.env.PORT || 3000 ; 
 
 
-
+let server;
 const startServer = async() => {
     try{ 
-
         await  connectDB() ;
-        app.listen(PORT , () => {
+        server = app.listen(PORT , () => {
           console.log(`Server is running on port ${PORT}`) ; 
        }) 
     }catch(err) {
@@ -18,7 +17,30 @@ const startServer = async() => {
     }
 }
 
-startServer();
+startServer(); 
 
+process.on("SIGINT", async () => {
+    console.log("Shutting down server...");
 
+    try {
+        // 1. Stop accepting new HTTP requests
+        if (server) {
+            server.close(() => {
+                console.log("HTTP server closed");
+            });
+        }
 
+        // 2. Close MongoDB
+        if (mongoose.connection.readyState === 1) {
+            await mongoose.connection.close();
+            console.log("MongoDB connection closed");
+        }
+
+        // 3. Exit
+        process.exit(0);
+
+    } catch (err) {
+        console.error("Error during shutdown:", err);
+        process.exit(1);
+    }
+});
